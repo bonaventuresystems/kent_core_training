a4b1fe9e4b77d53b1d25c43f0a41931f2e466bb2
bonaventuresystems <66865310+bonaventuresystems@users.noreply.github.com>
2026-02-23 11:01:14 +0530

    Updated
    Updated

---
+++ KentSolution/08DemoBasics/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace _08DemoBasics
+{
+    //public delegate bool MyDelegate(int i);
+
+    public delegate Q MyDelegate<P,Q>(P i);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //ArrayList arr = new ArrayList();
+            //  foreach (var item in arr)
+            //  {
+
+            //  }
+
+            //Week week = new Week();
+
+            //  foreach (string day in week)
+            //  {
+            //      Console.WriteLine(day);
+            //  }
+
+
+            //MyDelegate pointer = new MyDelegate(Check);
+            //bool result = Check(30);
+
+            //MyDelegate pointer = delegate (int i)
+            //                    {
+            //                        return i > 20;
+            //                    };
+
+            //MyDelegate pointer =  (i)=>
+            //                        {
+            //                            return i > 20;
+            //                        };
+
+            //bool result = pointer(30);
+            //Console.WriteLine(result);
+
+
+            //string str = "abc";
+
+            ////  bool result =     MyString.CheckForValidEmailAddress(str, 100);
+
+            //bool result = str.CheckForValidEmailAddress<string>(100);
+
+            //int i = 100;
+
+            //Console.WriteLine(result);
+
+
+            //MyDelegate pointer = new MyDelegate(Check);
+            //MyDelegate pointer = delegate (int i)
+            //                    {
+            //                        return i > 20;
+            //                    };
+
+            //MyDelegate pointer = (i)=>
+            //                        {
+            //                            return i > 20;
+            //                        };
+
+            //Func<int,bool> pointer = (i) =>
+            //                            {
+            //                                return i > 20;
+            //                            };
+
+            Expression<Func<int, bool>> tree = (i) => i > 20; //Create a tree
+
+            Func<int, bool> pointer = tree.Compile(); //Compile a tree
+
+
+
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //bool result = Check(30);
+            //bool result = pointer(30); //Create + Compile + Execute Expression Tree
+
+            bool result = pointer(30); //Execute Expression Tree
+            stopwatch.Stop();
+
+
+            Console.WriteLine(result);
+            Console.WriteLine("Time Taken = {0}", stopwatch.ElapsedTicks);
+
+            Console.ReadLine();
+        }
+
+
+
+        //public static bool Check(int i)
+        //{
+        //    return i > 20;
+        //}
+    }
+
+
+    public static class MyString
+    {
+        public static bool CheckForValidEmailAddress<T>(this T str, int i)
+        {
+            //return str.Contains("@");
+            return true;
+        }
+    }
+
+    //public static class MyString 
+    //{
+    //    public static bool CheckForValidEmailAddress(this string str, int i)
+    //    {
+    //        //return str.Contains("@");
+    //        return true;
+    //    }
+    //}
+
+    public class Week: IEnumerable
+    {
+        private string[] days = new string[] { "Mon", "Tue", "Wed","Thur", "Fri" };
+
+        public IEnumerator GetEnumerator()
+        {
+           for (int i = 0; i < days.Length; i++)
+            {
+               yield return days[i];
+            }
+        }
+    }
+
+
+}
---
+++ KentSolution/09DemoBasics/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _09DemoBasics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Emp> emps = new List<Emp>() 
+            {
+                new Emp(){ No = 11, Name = "Sachin1", Address = "Pune1"  },
+                 new Emp(){ No = 12, Name = "Sachin2", Address = "Pune2"  },
+                  new Emp(){ No = 13, Name = "Sachin3", Address = "Pune3"  },
+                   new Emp(){ No = 14, Name = "Sachin4", Address = "Pune4"  },
+                    new Emp(){ No = 15, Name = "Sachin5", Address = "Pune5"  },
+                     new Emp(){ No = 16, Name = "Sachin6", Address = "Pune6"  },
+                      new Emp(){ No = 17, Name = "Sachin7", Address = "Pune7"  },
+                       new Emp(){ No = 18, Name = "Sachin8", Address = "Pune8"  },
+            };
+
+            //List<Emp> result = new List<Emp>();
+            //foreach (var emp in emps)
+            //{
+            //    if (emp.No > 15)
+            //    {
+            //        result.Add(emp);
+            //    }
+            //}
+
+            //var result = (from emp in emps
+            //              where emp.No > 15
+            //              select emp);//.ToList();
+
+            var result = emps.Where((emp) => { return emp.No > 15; })
+                             .Select(emp => emp.Name)
+                             .ToList();
+
+            Console.WriteLine("Result is ");
+
+            emps.Add(new Emp() { No = 30, Name = "abc", Address = "Chennai" });
+            
+            foreach (var item in result)
+            {
+                Console.WriteLine( item);
+                //Console.WriteLine(item.No + item.Name);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    public class Emp
+    {
+        public int No { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
